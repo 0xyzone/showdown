cdb684b7727c9d2096f80b9850e874f5404541bd
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-08-06 14:34:31 +0545

    Update LeadsTable.php

--- app/Filament/Resources/Leads/Tables/LeadsTable.php
+++ app/Filament/Resources/Leads/Tables/LeadsTable.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\Leads\Tables;
 
+use App\Models\LeadStatus;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\SelectColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -18,8 +21,6 @@ class LeadsTable
                     ->searchable(),
                 TextColumn::make('lead_type.name')
                     ->searchable(),
-                TextColumn::make('lead_status.name')
-                    ->searchable(),
                 TextColumn::make('company_name')
                     ->searchable(),
                 TextColumn::make('contact_name')
@@ -28,6 +29,11 @@ class LeadsTable
                     ->searchable(),
                 TextColumn::make('email')
                     ->label('Email address')
+                    ->searchable(),
+                SelectColumn::make('lead_status_id')
+                    ->options(
+                        LeadStatus::all()->pluck('name', 'id')
+                    )
                     ->searchable(),
                 TextColumn::make('created_at')
                     ->dateTime()
@@ -42,6 +48,7 @@ class LeadsTable
                 //
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
             ])
             ->toolbarActions([
