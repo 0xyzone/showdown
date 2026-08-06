b37b18b36a3856bc226dc166a20bfd558d93f71d
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-08-06 13:37:55 +0545

    Create 2026_08_06_133643_change_gmaplink_type_in_leads_table.php

---
+++ database/migrations/2026_08_06_133643_change_gmaplink_type_in_leads_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->longText('gmap_link')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->string('gmap_link')->nullable()->change();
+        });
+    }
+};
